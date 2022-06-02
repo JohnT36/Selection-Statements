7c86cb7423028acae58b4fad286fdbe6c1c973b0
John Tuck <j.tuck1336@gmail.com>
2022-06-02 13:03:12 -0400

    selection Statements round 1

---
+++ Selection Statements/Selection Statements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Selection_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Welcome to the number guessing game!");
+            Console.WriteLine($"See if you can guess the correct number!");
+            Console.WriteLine($"Guess a number between 1-100");
+            
+            
+            var random = new Random();
+            var number = random.Next(1, 100);
+            int input;
+            do
+            {
+
+               input = int.Parse(Console.ReadLine());
+
+                if (input == number)
+                {
+                    Console.WriteLine($"You are correct! Well done");
+                }
+                else if (input > number)
+                {
+                    Console.WriteLine($"You went tooo high! Take it down a notch. Try again");
+                }
+                else
+                {
+                    Console.WriteLine($"You are too low! Pick it up! Try again");
+
+                }
+            } while (input != number);
+        }
+    }
+}
